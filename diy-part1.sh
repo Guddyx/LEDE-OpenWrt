be97ca1471960ca7a758d20882bed776280af83d
Guddyx <55354502+Guddyx@users.noreply.github.com>
2026-09-16 04:12:06 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 #sed -i 's#coolsnowwolf/luci#github用户名/luci#g' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git haiibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
 #echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git' >>feeds.conf.default
